778000ffd419f7e51d030509c7c471e0b22efe7e
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2020-11-23 22:27:33 +0100

    fixed wrong export

--- lib/src/dartemis_helpers.dart
+++ lib/src/dartemis_helpers.dart
@@ -2,5 +2,5 @@ export 'dartemis/components.dart';
 export 'dartemis/managers/camera_manager.dart';
 export 'dartemis/managers/view_projection_matrix_manager.dart';
 export 'dartemis/systems/simple_acceleration_system.dart';
-export 'dartemis/systems/simple_acceleration_system.dart';
 export 'dartemis/systems/simple_gravity_system.dart';
+export 'dartemis/systems/simple_movement_system.dart';
